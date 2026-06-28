c218bf13dbd0f875245067f9695aeb7b2c116a27
holawaleh <holawaleh@gmail.com>
2026-06-28 13:15:00 +0100

    k

--- shopkepa/settings/base.py
+++ shopkepa/settings/base.py
@@ -45,6 +45,13 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'shopkepa.urls'
 
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
